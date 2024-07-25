b2bab7a91f8d7488ecd070de9aadc8d8babc51ca
arthurgarcia22 <161314005+arthurgarcia22@users.noreply.github.com>
2024-07-25 01:42:25 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "cnhi-p-001-delivery.sitecorecontenthub.cloud",
+      "assets.revistacultivar.com.br",
+      "cerradocase.com.br",
+    ],
+  },
+};
 
 export default nextConfig;
